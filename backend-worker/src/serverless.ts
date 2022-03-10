5a76a0a8956a42ed2c97128d041a53dacaacfc29
Mathijs Vogelzang <mathijs@gmail.com>
2022-03-10 12:45:02 +0100

    Fix types in serverless

--- backend-worker/src/serverless.ts
+++ backend-worker/src/serverless.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
-
+import type { FastifyReply, FastifyRequest } from 'fastify';
 // Require the framework
 import Fastify from 'fastify';
-import router from '../dist/router';
+import router from './router';
+
 dotenv.config();
 
 // Instantiate Fastify with some config
@@ -12,7 +13,7 @@ const app = Fastify({
 
 app.register(router);
 
-export default async(req, res) => {
+export default async(req: FastifyRequest, res: FastifyReply) => {
   await app.ready();
   app.server.emit('request', req, res);
 };
