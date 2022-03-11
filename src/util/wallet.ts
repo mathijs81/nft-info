b08e8e66e21f8dd089a1ddbb7e2048ba87ab0469
Mathijs Vogelzang <mathijs@gmail.com>
2022-03-11 21:20:06 +0100

    Fix dependency conflicts

--- src/util/wallet.ts
+++ src/util/wallet.ts
@@ -1,43 +1,39 @@
 
-import type { ExternalProvider, JsonRpcProvider } from '@ethersproject/providers';
-import { Web3Provider } from '@ethersproject/providers';
-import detectEthereumProvider from '@metamask/detect-provider';
-import { ref, registerRuntimeCompiler } from 'vue';
-
 import { sequence } from '0xsequence';
-
 import Web3Modal from '@0xsequence/web3modal';
+import type { JsonRpcProvider } from '@ethersproject/providers';
 import { ethers } from 'ethers';
+import { ref } from 'vue';
 
-export class WalletService {
-  private externalProvider: ExternalProvider | null = null;
-  private provider: JsonRpcProvider | null = null;
+// export class WalletService {
+//   private externalProvider: ExternalProvider | null = null;
+//   private provider: JsonRpcProvider | null = null;
 
-  address = ref(null as string|null);
+//   address = ref(null as string|null);
 
-  async connect() {
-    this.externalProvider = await detectEthereumProvider() as ExternalProvider;
-    if (this.externalProvider) {
-      await this.externalProvider.request!({ method: 'eth_requestAccounts' });
-      this.provider = new Web3Provider(this.externalProvider);
-      const accounts = await this.provider.listAccounts();
-      if (accounts.length > 0)
-        this.address.value = accounts[0];
-      else
-        throw new Error('No accounts returned from metamask');
-    }
-    else {
-      throw new Error('No metamask found');
-    }
-  }
+//   async connect() {
+//     this.externalProvider = await detectEthereumProvider() as ExternalProvider;
+//     if (this.externalProvider) {
+//       await this.externalProvider.request!({ method: 'eth_requestAccounts' });
+//       this.provider = new Web3Provider(this.externalProvider);
+//       const accounts = await this.provider.listAccounts();
+//       if (accounts.length > 0)
+//         this.address.value = accounts[0];
+//       else
+//         throw new Error('No accounts returned from metamask');
+//     }
+//     else {
+//       throw new Error('No metamask found');
+//     }
+//   }
 
-  async signMessage(message: string): Promise<string> {
-    const signature = await this.provider?.getSigner().signMessage(message);
-    if (!signature)
-      throw new Error('Couldn\'t sign message');
-    return signature;
-  }
-}
+//   async signMessage(message: string): Promise<string> {
+//     const signature = await this.provider?.getSigner().signMessage(message);
+//     if (!signature)
+//       throw new Error('Couldn\'t sign message');
+//     return signature;
+//   }
+// }
 
 export class SequenceWalletService {
   private wallet: sequence.Wallet|null = null;
